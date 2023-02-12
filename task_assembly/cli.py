02bc3851ff81051baab6fd644f505cb1019e1eee
Dave Schultz <dave@daveschultzconsulting.com>
2023-02-11 18:24:29 -0800

    New config file support

--- task_assembly/cli.py
+++ task_assembly/cli.py
@@ -1,10 +1,12 @@
 import json
 import os.path
 import posixpath
+from pathlib import Path
 from datetime import datetime
 
 import larry as lry
 import argparse
+import toml
 import csv
 
 from botocore.exceptions import ClientError
@@ -319,10 +321,13 @@ class CLI:
 
 def main():
     parser = argparse.ArgumentParser("Task Assembly CLI")
+    parser.add_argument("--profile")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     c_parser = subparsers.add_parser("configure")
     c_parser.add_argument("--key")
+    c_parser.add_argument("--key_secret")
+    c_parser.add_argument("--aws_profile")
     c_parser.add_argument("--validate", action="store_true")
 
     ctt_parser = subparsers.add_parser("create_task_type")
@@ -416,19 +421,64 @@ def main():
 
     args = parser.parse_args()
 
-    if args.command == "configure" and args.key:
-        with open("api-key.txt", "w") as fp:
-            fp.write(args.key)
+    ta_dir = Path.home().joinpath(".taskassembly")
+    ta_config = ta_dir.joinpath("config.toml")
+    profile = args.profile if args.profile else "default"
+
+    if args.command == "configure" and (args.key or args.key_secret or args.aws_profile):
+        ta_dir.mkdir(exist_ok=True)
+        config = {"version": "0.1"}
+        if ta_config.exists():
+            with open(ta_config) as fp:
+                config = toml.load(fp)
+        if profile not in config:
+            config[profile] = {}
+        pf = config[profile]
+        if "credentials" not in pf:
+            pf["credentials"] = {}
+        creds = pf["credentials"]
+        if args.key:
+            creds["api_key"] = args.key
+        if args.key_secret:
+            creds["api_key_secret"] = args.key_secret
+        if args.aws_profile:
+            creds["aws_profile"] = args.aws_profile
+        with open(ta_config, "w") as fp:
+            toml.dump(config, fp)
         if not args.validate:
             exit(0)
 
-    if not os.path.exists("api-key.txt"):
-        print("No api-key.txt file found. Please run the configure command first.")
+    if not ta_config.exists():
+        print("No configuration file found. Please run the 'configure' command first.")
         exit(1)
 
-    with open("api-key.txt") as fp:
-        api_key = fp.read().strip()
-        client = AssemblyClient(api_key)
+    with open(ta_config) as fp:
+        config = toml.load(fp)
+        profile_config = config.get(profile)
+    if not profile_config:
+        print(f"No configuration found for {profile} profile")
+        exit(1)
+    profile_credentials = profile_config["credentials"]
+    if profile_credentials.get("aws_profile"):
+        lry.set_session(profile_name=profile_credentials.get("aws_profile"))
+    api_key = None
+    if "api_key" in profile_credentials:
+        api_key = profile_credentials.get("api_key")
+    elif "api_key_secret" in profile_credentials:
+        sm = lry.session().client('secretsmanager')
+        response = sm.get_secret_value(SecretId=profile_credentials["api_key_secret"])
+        secret_value = response["SecretString"]
+        try:
+            secrets = json.loads(secret_value)
+            api_key = secrets["api_key"]
+        except:
+            api_key = secret_value
+
+    if api_key is None:
+        print("Missing api key value")
+        exit(1)
+
+    client = AssemblyClient(api_key)
     cli = CLI(client)
 
     if args.command == "configure" and args.validate:
