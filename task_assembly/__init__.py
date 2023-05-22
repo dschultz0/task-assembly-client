cd2470a10a797a4d2a7a1b8df9956694d8ebaedc
Dave Schultz <dave@daveschultzconsulting.com>
2023-05-22 10:40:05 -0700

    Add version number

--- task_assembly/__init__.py
+++ task_assembly/__init__.py
@@ -1,1 +1,6 @@
+import importlib.metadata
 from .client import AssemblyClient, Task
+try:
+    __version__ = importlib.metadata.version("task-assembly")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "local"
